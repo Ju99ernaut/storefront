9eb70124601639471073c0cd7a42d7e5f7529091
Ju99ernaut <brendonzee@gmail.com>
2021-05-06 22:56:01 +0200

    Add icons

---
+++ components/icons/index.js
@@ -0,0 +1,6 @@
+export { default as Bag } from './Bag';
+export { default as Check } from './Check';
+export { default as Cross } from './Cross';
+export { default as Plus } from './Plus';
+export { default as Minus } from './Minus';
+export { default as Trash } from './Trash';
